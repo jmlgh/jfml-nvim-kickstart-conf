f34c8d2133a1161a851f32f13a719fb4bfae8461
Javier Martinez Lizama <jfmarli@gmail.com>
2024-03-31 17:41:26 +0200

    adds custom keymaps and nvim-tree

--- init.lua
+++ init.lua
@@ -538,6 +538,7 @@ require('lazy').setup({
       local servers = {
         clangd = {},
         gopls = {},
+        tsserver = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -850,6 +851,33 @@ require('lazy').setup({
     },
   },
 })
+
+-- Custom keymaps
+-- Space + a saves the file
+vim.keymap.set('n', '<Leader>a', ':write<CR>', { silent = true })
+-- Move to first symbol on the line
+vim.keymap.set('n', 'H', '^')
+-- Move to last symbol of the line
+vim.keymap.set('n', 'L', '$')
+-- Shift + q - Quit
+vim.keymap.set('n', 'Q', '<C-W>q')
+-- vv - Makes vertical split
+vim.keymap.set('n', 'vv', '<C-W>v')
+-- ss - Makes horizontal split
+vim.keymap.set('n', 'ss', '<C-W>s')
+-- Indenting in visual mode (tab/shift+tab)
+vim.keymap.set('v', '<Tab>', '>gv')
+vim.keymap.set('v', '<S-Tab>', '<gv')
+-- Move to the end of yanked text after yank and paste
+vim.cmd 'vnoremap <silent> y y`]'
+vim.cmd 'vnoremap <silent> p p`]'
+vim.cmd 'nnoremap <silent> p p`]'
+-- Space + h to clean search highlight
+vim.keymap.set('n', '<Leader>h', ':noh<CR>', { silent = true })
+-- Fixes pasting after visual selection.
+vim.keymap.set('v', 'p', '"_dP')
+
+---------- end of custom keymaps -----
 
 -- Debugging
 vim.keymap.set('n', '<F5>', ":lua require'dap'.continue()<CR>")
