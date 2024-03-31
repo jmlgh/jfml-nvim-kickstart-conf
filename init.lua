2116e250f86fcf82913fa93642dd4bd8857507e3
Javier Martinez Lizama <jfmarli@gmail.com>
2024-03-31 21:23:04 +0200

    adds custom keymaps for nvim-cmp

--- init.lua
+++ init.lua
@@ -654,6 +654,12 @@ require('lazy').setup({
       local luasnip = require 'luasnip'
       luasnip.config.setup {}
 
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
+      end
+
       cmp.setup {
         snippet = {
           expand = function(args)
@@ -676,6 +682,35 @@ require('lazy').setup({
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
           ['<C-y>'] = cmp.mapping.confirm { select = true },
+
+          -- Accept ([y]es) the completion.
+          --  This will auto-import if your LSP supports it.
+          --  This will expand snippets if the LSP sent a snippet.
+          ['<CR>'] = cmp.mapping.confirm { select = true },
+
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+            -- that way you will only jump inside the snippet region
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
 
           -- Manually trigger a completion from nvim-cmp.
           --  Generally you don't need this, because nvim-cmp will display
@@ -701,6 +736,7 @@ require('lazy').setup({
             end
           end, { 'i', 's' }),
         },
+        -- sources for autocompletion
         sources = {
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
@@ -877,8 +913,6 @@ vim.keymap.set('n', '<Leader>h', ':noh<CR>', { silent = true })
 -- Fixes pasting after visual selection.
 vim.keymap.set('v', 'p', '"_dP')
 
----------- end of custom keymaps -----
-
 -- Debugging
 vim.keymap.set('n', '<F5>', ":lua require'dap'.continue()<CR>")
 vim.keymap.set('n', '<F3>', ":lua require'dap'.step_over()<CR>")
@@ -904,8 +938,6 @@ end
 dap.listeners.before.event_exited['dapui_config'] = function()
   dapui.close()
 end
-
---------- end Debugging options -----------------------
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
