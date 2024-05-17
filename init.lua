9b6d1770991cd92c840407bffe61b65b3965d5ac
Javi Martinez Lizama <jfmarli@gmail.com>
2024-05-17 21:34:05 +0200

    adds templ as supported filetype extension. INSTALL: :TSInstall templ and html-lsp, htmx-lsp (from Mason)

--- init.lua
+++ init.lua
@@ -945,5 +945,7 @@ dap.listeners.before.event_exited['dapui_config'] = function()
   dapui.close()
 end
 
+vim.filetype.add { extension = { templ = 'templ' } }
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
